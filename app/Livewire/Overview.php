5d3e3b7e2df8df29324015334e85bba97ea10abc
Aditya Muhammad Zaini <adityamzainir@gmail.com>
2025-07-31 16:29:10 +0700

    feat(): test overview components

---
+++ app/Livewire/Overview.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Overview extends Component
+{
+    public $title = 'Overview';
+
+    public function render()
+    {
+        return view('livewire.overview');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\Overview;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/overview', Overview::class);
